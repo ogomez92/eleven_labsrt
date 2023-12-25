7244c69089e6f06a25b74f0e382e518847ee84d7
Oriol <ogomez.s92@gmail.com>
2023-12-25 19:35:56 +0100

    fix: fix contradictory help message in cli parser

--- src/cli_parser.py
+++ src/cli_parser.py
@@ -8,8 +8,8 @@ def get_args():
     parser.add_argument('-i', '--input-file', help='Input SRT file to be processed.', type=str)
     parser.add_argument('-d', '--debug', help='Debug/verbose mode.', action='store_true')
     parser.add_argument('-v', '--voice', help='Voice to use when converting subtitles. Must be a valid voice on the ElevenLabs user account whose API key is used', type=str)
-    parser.add_argument('-p', '--prefer-speedup', help='Prefer speeding up audio to fit overlapping subtitles. If neither --prefer-speedup nor --prefer-queue is specified, queue is used.', action='store_true')
-    parser.add_argument('-q', '--prefer-queue', help='Place overlapping subtitles sequentially. If this nor prefer speed up is specified, the app will ask.', action='store_true')
+    parser.add_argument('-s', '--prefer-speedup', help='Prefer speeding up audio to fit overlapping subtitles. If neither --prefer-speedup nor --prefer-queue is specified, queue is used.', action='store_true')
+    parser.add_argument('-q', '--prefer-queue', help='Place overlapping subtitles sequentially. If this nor --prefer-speedup is specified, the app will use this by default.', action='store_true')
 
     # Return the parsed arguments
     return parser.parse_args()
