e5ebf2f83f476f8fd542c90ad1db4180ee49007b
Oriol <ogomez.s92@gmail.com>
2023-12-25 20:16:35 +0100

    add output file argument

--- src/cli_parser.py
+++ src/cli_parser.py
@@ -10,6 +10,7 @@ def get_args():
     parser.add_argument('-v', '--voice', help='Voice to use when converting subtitles. Must be a valid voice on the ElevenLabs user account whose API key is used', type=str)
     parser.add_argument('-s', '--prefer-speedup', help='Prefer speeding up audio to fit overlapping subtitles. If neither --prefer-speedup nor --prefer-queue is specified, queue is used.', action='store_true')
     parser.add_argument('-q', '--prefer-queue', help='Place overlapping subtitles sequentially. If this nor --prefer-speedup is specified, the app will use this by default.', action='store_true')
+    parser.add_argument('-o', '--output-file', help='Output wave file to generate (use ffmpeg to save to mp3).', type=str)
 
     # Return the parsed arguments
     return parser.parse_args()
